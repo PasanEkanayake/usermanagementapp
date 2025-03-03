99436156d2551c7d59ff861832a3363e16f73707
Pasan Ekanayake <pasanekanayake@gmail.com>
2025-03-04 00:25:17 +0530

    commit

--- app/src/main/java/com/example/usermanagementapp/MainActivity.java
+++ app/src/main/java/com/example/usermanagementapp/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.usermanagementapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +12,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button regBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +26,17 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
+        regBtn = findViewById(R.id.register);
 
+        regBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent =new Intent(MainActivity.this, RegistrationForm.class);
+                startActivity(intent);
+
+            }
+        });
 
     }
 }
